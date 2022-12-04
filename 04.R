b8554bff139ab7fcf3557c7adec23509202f0cee
jasmineirx <jasmine@insight-rx.com>
2022-12-04 12:49:49 -0500

    day 4 solution

---
+++ 04.R
@@ -0,0 +1,16 @@
+inp <- readLines("04.txt", warn = FALSE)
+
+parse_ranges <- function(s) {
+  m <- regexpr("^([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)$", s, perl = TRUE)
+  matrix(
+    as.numeric(
+      substring(s,x <- attr(m,"capture.start"), attr(m,"capture.length") + x-1) 
+    ),
+    nrow = length(s)
+  )
+}
+contains <- function(r) {(r[1]<=r[3] & r[2]>=r[4]) | (r[3]<=r[1] & r[4]>=r[2])}
+overlaps <- function(r) {length(intersect(seq(r[1],r[2]), seq(r[3],r[4]))) >= 1}
+
+message("Part 1: ", sum(apply(parse_ranges(inp), 1, contains)))
+message("Part 2: ", sum(apply(parse_ranges(inp), 1, overlaps)))
