a97536dabb06d9ecd634a251cd0519a70a757fff
jasmineirx <jasmine@insight-rx.com>
2022-12-08 01:35:24 -0500

    tidier, one loop

--- 08.R
+++ 08.R
@@ -1,55 +1,32 @@
-inp <- readLines("07.txt", warn = FALSE)
+inp <- strsplit(readLines("08.txt", warn = FALSE), "")
+d <- length(inp)
+m <- inp |>
+  unlist() |>
+  as.numeric() |>
+  matrix(byrow = T, nrow = d)
+dir_score <- function(v, tree) { 
+  ifelse(any(v >= tree), length(v[1:which(v >= tree)[1]]), length(v))
+} 
 
-fps <- function(fp) paste0(fp, collapse = "_")
-
-get_dir_structure <- function(inp) {
-  instr <- which(grepl("\\$ ", inp))[-1]
-  dirs <- c(top = 0)
-  fp <- c("top")
-  for (i in instr) {
-    command <- inp[i]
-    if (grepl(" ls$", command)) {
-      ls_out <- inp[(i+1):(pmin(instr[instr > i][1] - 1, length(inp), na.rm=T))]
-      new_dirs <- gsub("dir ", "", ls_out[grepl("^dir" , ls_out)])
-      if (length(new_dirs) > 0) {
-        new_path <- paste0(fps(fp), "_", new_dirs)
-        dirs <- c(dirs, setNames(rep(0, length(new_dirs)), new_path))
-      }
-      new_files <- as.numeric(gsub(" .*", "", ls_out[!grepl("^dir" , ls_out)]))
-      dirs[fps(fp)] <- sum(new_files)
-      
-    } else {
-      new_path <- gsub("\\$ cd ", "",command)
-      if (new_path == "..") {
-        fp <- fp[-length(fp)]
-      } else {
-        fp <- c(fp, new_path)
-      }
-    }
+vis <- matrix(rep(TRUE, d * d), nrow = d)
+scenic <- matrix(rep(0, d * d), nrow = d)
+for (i in 2:(d - 1)) {
+  for (j in 2:(d - 1)) {
+    tree <- m[i, j]
+    vis[i,j] <- any(c(
+      all(m[1:(i-1), j] < tree), 
+      all(m[(i+1):d, j] < tree), 
+      all(m[i, 1:(j-1)] < tree), 
+      all(m[i, (j+1):d] < tree)
+    )) 
+    scenic[i,j] <- prod(c(
+      dir_score(rev(m[1:(i-1), j]), tree), 
+      dir_score(m[(i+1):d, j], tree), 
+      dir_score(rev(m[i, 1:(j-1)]), tree), 
+      dir_score(m[i, (j+1):d], tree)
+    )) 
   }
-  dirs
 }
 
-sum_of_small_dirs <- function(dirs, lim = 100000) {
-  tot <- 0
-  for (nm in names(dirs)) {
-    sub_dirs <- c(nm, names(dirs)[grepl(paste0("^", nm, "_"), names(dirs))])
-    dir_tot <- sum(dirs[sub_dirs])
-    if (dir_tot < lim) tot <- tot + dir_tot
-  }
-  tot
-}
-
-dir_to_delete <- function(dirs, must_delete) {
-  opts <- setNames(rep(0, length(dirs)), names(dirs))
-  for (nm in names(dirs)) {
-    sub_dirs <- c(nm, names(dirs)[grepl(paste0("^", nm, "_"), names(dirs))])
-    dir_tot <- sum(dirs[sub_dirs])
-    opts[nm] <- dir_tot
-  }
-  min(opts[opts > must_delete])
-}
-
-dirs <- get_dir_structure(inp)
-message("Part 1: ", sum_of_small_dirs(dirs))
-message("Part 2: ", dir_to_delete(dirs, 30000000 - (70000000 - sum(dirs))))
+message("Part 1: ", sum(vis))
+message("Part 2: ", max(scenic))
