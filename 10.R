1b63e9b546a27b5d96e6fa2cae120ec202ac5839
jasmineirx <jasmine@insight-rx.com>
2022-12-11 18:32:54 -0500

    move around arguments

--- 10.R
+++ 10.R
@@ -1,23 +1,20 @@
-inp <- strsplit(readLines("09.txt", warn = FALSE), " ")
-key <- list(R = c(1, 0), L = c(-1, 0), U = c(0, 1), D = c(0, -1))
-save_pos <- function(pos, seen) {unique(c(seen, paste0(pos[1], "_", pos[2])))}
+inp <- strsplit(readLines("10.txt", warn = FALSE), " ")
+which_cycles <- c(20, 60, 100, 140, 180, 220)
+m <- as.data.frame(Reduce(rbind, lapply(inp, `length<-`, 2)), row.names = FALSE)
+m[,1] <- cumsum((m[,1] == "addx") + 1)
+midcycles <- setdiff(1:240, m[,1])
+m <- rbind(m, matrix(c(midcycles, rep(0, length(midcycles))), ncol = 2))
+m <- m[order(m[,1]),]
+m[is.na(m[,2]),2] <- 0
+m[,2] <- c(0, cumsum(m[,2])[-nrow(m)]) + 1
+m[,3] <- (m[,1] - 1) %% 40
 
-knotted_string <- function(inp, nk, k = key) {
-  pos <- lapply(1:nk, function(x) c(0, 0))
-  seen <- save_pos(pos[[nk]], c())
-  
-  for (i in inp) {
-    for (step in 1:as.numeric(i[2])) {
-      pos[[1]] <- pos[[1]] + k[[i[1]]]
-      for (knot in 2:nk) {
-        taught <- pos[[knot - 1]] - pos[[knot]]
-        pos[[knot]] <- pos[[knot]] + sign(taught) * any(abs(taught) > 1)
-      }
-      seen <- save_pos(pos[[nk]], seen)
-    }
-  }
-  save_pos(pos[[nk]], seen)
+print_crt <- function(sprite) {
+  sapply(
+    split(sprite, rep(1:6, each = 40)), 
+    function(x) paste0(c(".", "#")[x+1], collapse = "")
+  )
 }
 
-message("Part 1: ", length(knotted_string(inp, 2)))
-message("Part 2: ", length(knotted_string(inp, 10)))
+message("Part 1: ", sum(m[which_cycles, 1] * m[which_cycles, 2]))
+message("Part 2: ", cat(print_crt(abs(m[,2] - m[,3]) <= 1), sep = "\n"))
