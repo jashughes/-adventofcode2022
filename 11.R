2bf758303ae24c06929ea9b8699113343b665365
jasmineirx <jasmine@insight-rx.com>
2022-12-13 17:56:57 -0500

    one function for both

--- 11.R
+++ 11.R
@@ -8,35 +8,12 @@ test <- extract_num(inp[which(grepl("Test", inp))])
 ifT <- extract_num(inp[which(grepl("If true", inp))]) + 1
 ifF <- extract_num(inp[which(grepl("If false", inp))]) + 1
 
-inspect <- function(old, op, modby = 3, new = NULL) {
-  eval(parse(text = op))
-  floor(new/modby)
-} 
+inspect <- function(old, op, new = NULL) eval(parse(text = op))
 
-play1 <- function(inv, op, test, ifT, ifF, reps = 20, modby = 3){
+play <- function(inv, op, test, ifT, ifF, f, val, reps = 20){
   counts <- rep(0, length(op))
-  
-  for (r in 1:reps) {
-    for (monkey in 1:length(op)) {
-      items <- inv[[monkey]]
-      inv[[monkey]] <- numeric(0)
-      if (length(items) > 0) {
-        for (i in 1:length(items)) {
-          item <- inspect(items[i], op[monkey], modby)
-          counts[monkey] <- counts[monkey] + 1
-          pass_to <- ifelse(item %% test[monkey] == 0, ifT[monkey], ifF[monkey])
-          inv[[pass_to]] <- c(inv[[pass_to]], item)
-        }
-      }
-    }
-  }
-  list(counts = counts, inv = inv)
-}
-
-
-play2 <- function(inv, op, test, ifT, ifF, reps = 20, modby = 3){
-  counts <- rep(0, length(op))
-  iv <- lapply(inv, lapply, function(x) x %% test)
+  if (length(val) == 1) val <- rep(val, length(op))
+  iv <- lapply(inv, lapply, f, val)
   
   for (r in 1:reps) {
     for (monkey in 1:length(op)) {
@@ -44,9 +21,9 @@ play2 <- function(inv, op, test, ifT, ifF, reps = 20, modby = 3){
       iv[[monkey]] <- list()
       if (length(items) > 0) {
         for (i in 1:length(items)) {
-          item <- inspect(items[[i]], op[monkey], modby) %% test
+          item <- f(inspect(items[[i]], op[monkey]), val)
           counts[monkey] <- counts[monkey] + 1
-          pass_to <- ifelse(item[monkey] == 0, ifT[monkey], ifF[monkey])
+          pass_to <- ifelse(item[monkey] %% test[monkey] == 0, ifT[monkey], ifF[monkey])
           iv[[pass_to]] <- c(iv[[pass_to]], list(item))
         }
       }
@@ -55,8 +32,9 @@ play2 <- function(inv, op, test, ifT, ifF, reps = 20, modby = 3){
   counts
 }
 
-counts1 <- play1(inv, op, test, ifT, ifF, 20, 3)
-counts2 <- play2(inv, op, test, ifT, ifF, 10000, 1)
 
+
+counts1 <- play(lapply(inv, `*`, 3), op, test, ifT, ifF, `%/%`, 3, 20)
+counts2 <- play(inv, op, test, ifT, ifF, `%%`, test, 10000)
 message("Part 1: ", prod(sort(counts1, decreasing = TRUE)[1:2]))
 message("Part 2: ", prod(sort(counts2, decreasing = TRUE)[1:2]))
