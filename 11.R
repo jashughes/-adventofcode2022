f537719b962332d5ea654b389c08c03144c030ce
jasmineirx <jasmine@insight-rx.com>
2022-12-13 18:17:17 -0500

    parsing to play function

--- 11.R
+++ 11.R
@@ -1,9 +1,16 @@
 extract_num <- function(s) as.numeric(gsub(".* ([0-9]+)", "\\1", s))
 inspect <- function(old, op, new = NULL) eval(parse(text = op))
-top2 <- function(vec) prod(sort(vec, decreasing = TRUE)[1:2])
+top2prod <- function(vec) prod(sort(vec, decreasing = TRUE)[1:2])
 pass_test <- function(n, div, if_T, if_F) ifelse(n %% div == 0, if_T, if_F)
 
-play <- function(inv, op, test, ifT, ifF, f, reps = 20){
+play <- function(inp, f, reps = 20){
+  inv <- gsub(".*: ","", inp[which(grepl("Starting", inp))]) |>
+    strsplit(", ") |>
+    lapply(as.numeric)
+  op <- gsub(".*: ","", inp[which(grepl("Operation", inp))]) 
+  test <- extract_num(inp[which(grepl("Test", inp))])
+  ifT <- extract_num(inp[which(grepl("If true", inp))]) + 1
+  ifF <- extract_num(inp[which(grepl("If false", inp))]) + 1
   counts <- rep(0, length(op))
   iv <- lapply(inv, lapply, rep, length(op))
   
@@ -24,13 +31,7 @@ play <- function(inv, op, test, ifT, ifF, f, reps = 20){
   counts
 }
 inp <- readLines("11.txt", warn = FALSE)
-inv <- gsub(".*: ","", inp[which(grepl("Starting", inp))]) |>
-  strsplit(", ") |>
-  lapply(as.numeric)
-op <- gsub(".*: ","", inp[which(grepl("Operation", inp))]) 
 test <- extract_num(inp[which(grepl("Test", inp))])
-ifT <- extract_num(inp[which(grepl("If true", inp))]) + 1
-ifF <- extract_num(inp[which(grepl("If false", inp))]) + 1
 
-message("Part 1:", top2(play(inv,op,test,ifT,ifF, function(x) x %/% 3, 20)))
-message("Part 2:", top2(play(inv,op,test,ifT,ifF, function(x) x%%test, 10000)))
+message("Part 1: ", top2prod(play(inp, function(x) x %/% 3, 20)))
+message("Part 2: ", top2prod(play(inp, function(x) x %% test, 10000)))
