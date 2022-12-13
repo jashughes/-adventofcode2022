98d5155182dea03dfabda5ca00018ad9143a7317
jasmineirx <jasmine@insight-rx.com>
2022-12-13 11:55:30 -0500

    reuse functions

--- 07.R
+++ 07.R
@@ -1,55 +1,33 @@
 inp <- readLines("07.txt", warn = FALSE)
 
 fps <- function(fp) paste0(fp, collapse = "_")
+path <- function(fp, nw) if (nw == "..") { fp[-length(fp)] } else { c(fp, nw) }
 
 get_dir_structure <- function(inp) {
-  instr <- which(grepl("\\$ ", inp))[-1]
   dirs <- c(top = 0)
   fp <- c("top")
-  for (i in instr) {
-    command <- inp[i]
-    if (grepl(" ls$", command)) {
-      ls_out <- inp[(i+1):(pmin(instr[instr > i][1] - 1, length(inp), na.rm=T))]
-      new_dirs <- gsub("dir ", "", ls_out[grepl("^dir" , ls_out)])
-      if (length(new_dirs) > 0) {
-        new_path <- paste0(fps(fp), "_", new_dirs)
-        dirs <- c(dirs, setNames(rep(0, length(new_dirs)), new_path))
-      }
-      new_files <- as.numeric(gsub(" .*", "", ls_out[!grepl("^dir" , ls_out)]))
-      dirs[fps(fp)] <- sum(new_files)
-      
-    } else {
-      new_path <- gsub("\\$ cd ", "",command)
-      if (new_path == "..") {
-        fp <- fp[-length(fp)]
-      } else {
-        fp <- c(fp, new_path)
-      }
+  for (command in inp[-1]) {
+    if (grepl("^dir", command)) {
+      dirs <- c(dirs, setNames(0, fps(c(fp, gsub("^dir ", "", command)))))
+    } else if (grepl("^[0-9]+ .*", command)) {
+      dirs[fps(fp)] <- dirs[fps(fp)] + as.numeric(gsub(" .*", "", command))
+    } else if (grepl("\\$ cd ", command)) {
+      fp <- path(fp, gsub("\\$ cd ", "",command))
     }
   }
   dirs
 }
 
-sum_of_small_dirs <- function(dirs, lim = 100000) {
-  tot <- 0
-  for (nm in names(dirs)) {
-    sub_dirs <- c(nm, names(dirs)[grepl(paste0("^", nm, "_"), names(dirs))])
-    dir_tot <- sum(dirs[sub_dirs])
-    if (dir_tot < lim) tot <- tot + dir_tot
-  }
-  tot
-}
-
-dir_to_delete <- function(dirs, must_delete) {
+dir_sizes <- function(dirs, lim = 100000) {
   opts <- setNames(rep(0, length(dirs)), names(dirs))
   for (nm in names(dirs)) {
     sub_dirs <- c(nm, names(dirs)[grepl(paste0("^", nm, "_"), names(dirs))])
-    dir_tot <- sum(dirs[sub_dirs])
-    opts[nm] <- dir_tot
+    opts[nm]<- sum(dirs[sub_dirs])
   }
-  min(opts[opts > must_delete])
+  opts
 }
 
 dirs <- get_dir_structure(inp)
-message("Part 1: ", sum_of_small_dirs(dirs))
-message("Part 2: ", dir_to_delete(dirs, 30000000 - (70000000 - sum(dirs))))
+sz <- dir_sizes(dirs)
+message("Part 1: ", sum(sz[sz < 100000]))
+message("Part 2: ", min(sz[sz > 30000000 - (70000000 - sum(dirs))]))
