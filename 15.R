034189cd9ef8188f96f44f9fa4e5630a0d37bd8a
jasmineirx <jasmine@insight-rx.com>
2022-12-15 22:50:45 -0500

    reduce memory reallocations

--- 15.R
+++ 15.R
@@ -7,21 +7,33 @@ sb <- regmatches(inp, gregexpr('\\(?[0-9-]+', inp)) |>
 overlaps <- function(a1,a2,b1,b2) {(a1<=b1 & a2>=b1) | (b1<=a1 & b2>=a1)}
 signal_strength <- function(v) v[1] * 4000000 + v[2]
 Nblock <- function(r,s,y) sum(r[,2]-r[,1])+nrow(r)-nrow(unique(s[s[,4]==y,3:4]))
-manhattan <- function(m) abs(m[3] - m[1]) + abs(m[4] - m[2])
+manhattan <- function(m) abs(m[,3] - m[,1]) + abs(m[,4] - m[,2])
 
 blocked_on_y <- function(sb, y = 2000000) {
-  dx <- apply(sb,1, manhattan) - abs(sb[,2] - y)
-  itxm <- matrix(sb[which(dx > 0),1] + c(-dx[dx>0], dx[dx>0]), ncol = 2)
-  ranges <- itxm[1,, drop = F]
+  dx <- manhattan(sb) - abs(sb[,2] - y)
+  itxm <- matrix(rep(0, 2 * sum(dx > 0)), ncol = 2)
+  itxm[,1] <- sb[dx > 0, 1] - dx[dx > 0]
+  itxm[,2] <- sb[dx > 0, 1] + dx[dx > 0]
+  
+  ranges <- matrix(nrow = nrow(sb), ncol = 2)
+  nr <- 1
+  b <- numeric(2)
+  
+  ranges[1,] <- itxm[1,]
   for (r in 2:nrow(itxm)) { 
-    b <- itxm[r,,drop=F]
-    ol <- overlaps(ranges[,1], ranges[,2], b[,1], b[,2])
-    ranges <- rbind(
-      ranges[!ol,,drop=F],
-      cbind(min(ranges[ol,1], b[,1]), max(ranges[ol,2], b[,2]))
-    )
+    b[] <- itxm[r,]
+    ol <- overlaps(ranges[seq_len(nr),1], ranges[seq_len(nr),2], b[1], b[2])
+    if (any(ol)) {
+      b <- c(min(ranges[seq_len(nr)[ol],1], b[1]), max(ranges[seq_len(nr)[ol],2], b[2]))
+      ranges[seq_len(sum(!ol)),] <- ranges[seq_len(nr)[!ol],]
+      nr <- sum(!ol) + 1
+      ranges[nr,] <- b
+    } else {
+      nr <- nr + 1
+      ranges[nr,] <- b
+    }
   }
-  ranges
+  ranges[1:nr,,drop=FALSE]
 }
 
 find_location <- function(sb, y_min = 0, y_max = 4000000) {
@@ -35,4 +47,3 @@ find_location <- function(sb, y_min = 0, y_max = 4000000) {
 
 message("Part 1: ", Nblock(blocked_on_y(sb, 2000000), sb, y = 2000000))
 message("Part 2: ", signal_strength(find_location(sb)))
-
